6646f101356f20a87e915fa4c211b9b9fe9e3468
khadga <khadga@example.com>
2024-03-21 21:30:42 -0500

    git jar pushed

---
+++ Module_2/JarPracticeFiles/ThapaJarTwo.java
@@ -0,0 +1,9 @@
+package Module_2.JarPracticeFiles;
+public class ThapaJarTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello, This is ThapaJarTwo");		
+	}
+
+}
